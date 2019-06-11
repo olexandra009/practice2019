2b0507a63ba0f8255967255fd03f60d9f7ebcf4b
Alexandra <a.r.chumachenko@gmail.com>
2019-06-11 15:39:47 +0300

    Final commit

--- src/main_view/MainMenu.java
+++ src/main_view/MainMenu.java
@@ -1,5 +1,6 @@
 package main_view;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -33,6 +34,7 @@ public class MainMenu extends JPanel  {
 	
 	private Pixi pixiGame;
 	
+	private boolean help = false;
 	
 	/**Конструктор*/
 	public MainMenu(Pixi pixi2) {
@@ -60,9 +62,36 @@ public class MainMenu extends JPanel  {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		drawPixi(g);
+		if(help) {
+			drawHelp(g);
+		}
+	}
+	private void drawHelp(Graphics g) {
+		g.setColor(new Color(13, 13, 13, 200));
+	    g.fillRect(0, 0, 1010, 650);
+	    g.fillRect(90, 70, 800, 550);
+	    g.setColor(Color.white);
+	    g.setFont(new Font("Centry-Schoolbook", Font.PLAIN, 30));
+	    g.drawString("Гра \"Pixi\"", 140, 100);
+	    g.setFont(new Font("Centry-Schoolbook", Font.PLAIN, 20));
+	    g.drawString("(для продовження гри натисніть F1)", 100, 160);
+	    g.drawString("Головна мета гри набрати як можна більше балів та виростити Піксі", 110, 220);
+	    g.drawString("Бали набираються при проходженні міні ігор", 100, 250);
+	    g.drawString("Гра \"Встигни-порахуй\"", 100, 280);
+	    g.drawString("Необхідно правильно порахувати приклади та ввести з клавіатури результати", 110, 310);
+	    g.drawString("Гра \"Розкидане слово\"", 100, 340);
+	    g.drawString("Необхідно зібрати слово, яке зображує картинка. Гра триває одну хвилину.", 110, 370);
+	    g.drawString("Гра \"Трикутне коло\"", 100, 400);
+	    g.drawString("Необхідно обрати всі фігури які задовольняють умові", 110, 430);
+	    g.drawString("Гра \"Запам'ятай ліхтар\"", 100, 460);
+	    g.drawString("Необхідно запам'ятати послідовність підсвічення клітинок і повторити її", 110, 490);
+	    g.drawString("При набранні певної кількості балів персонаж змінюється", 100, 520);
+	    g.drawString("Бажаємо весело та корисно провести час!", 100, 590);
+	    
 	}
 	/**Метод, що малює головне вікно*/
 	private void drawPixi(Graphics g) {
+		g.setColor(Color.BLACK);
 		if(pixi_level!=3&&score>pixiScore[pixi_level])
 			pixi_level++;
 		g.drawImage(pixi[pixi_level], 0, 0, null);
@@ -72,6 +101,9 @@ public class MainMenu extends JPanel  {
 		g.drawImage(word, boardWidht-300, 210, null);
 		g.drawImage(figure, boardWidht-300, 320, null);
 		g.drawImage(light, boardWidht-300, 430, null);
+		g.setColor(new Color(13,13,13,110));
+		g.setFont(new Font("Centry-Shoolbook", Font.PLAIN, 10));
+		g.drawString("Для допомоги натисніть F1", 0, 640);
 	}
 	/**Зміна ігор*/
 	private void changePanel(int x, int y) {
@@ -105,17 +137,24 @@ class MyMouseListner extends MouseAdapter {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+	   if(!help)
 		changePanel(e.getX(), e.getY());
 		
 	}
 
 }
+
 class MyKeyListner extends KeyAdapter{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
               if(e.isShiftDown()&&e.isControlDown()&&e.getKeyCode() == KeyEvent.VK_F5) {
             	  score+=100;
+            	  repaint();
+              }
+              if(e.getKeyCode() == KeyEvent.VK_F1)
+              {
+            	  help = !help;
             	  repaint();
               }
 	
